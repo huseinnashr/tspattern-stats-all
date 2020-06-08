68953fd61d4fce90945f610b1fab5dc29ad33762
Husein <nvdhunter007@gmail.com>
2020-06-09 00:49:15 +0700

    summary: add run analysis & print report func

--- src/Summary.ts
+++ src/Summary.ts
@@ -1,6 +1,12 @@
 import { Analyzer } from "./analyzers/Analyzer";
 import { OutputTarget } from "./outputTargets/OutputTarget";
+import { MatchData } from "./MatchData";
 
 export class Summary {
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
