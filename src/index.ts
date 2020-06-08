82fe74f5777343778ca0f784bf59c3feda5e30e7
Husein <nvdhunter007@gmail.com>
2020-06-09 00:49:39 +0700

    summary: try it in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-import { CsvFileReader } from "./CsvFileReader";
+import { CsvFileReader } from "./dataReaders/CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResults";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { ConsoleReport } from "./outputTargets/ConsoleReport";
+import { Summary } from "./Summary";
 
 const reader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(reader);
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
 
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
